365cc5a72a217bdc8288c65790e6b997f1a0dcd2
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2026-06-06 14:02:24 +0530

    feat: implement GuestController and GuestService for managing guest data operations

--- guest-service/src/main/java/com/hotel/guest_service/controller/GuestController.java
+++ guest-service/src/main/java/com/hotel/guest_service/controller/GuestController.java
@@ -1,4 +1,28 @@
 package com.hotel.guest_service.controller;
 
+import com.hotel.guest_service.model.Guest;
+import com.hotel.guest_service.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/guests")
 public class GuestController {
+
+    @Autowired
+    private GuestService guestService;
+
+    // POST request: http://localhost:8082/api/guests
+    @PostMapping
+    public Guest createGuest(@RequestBody Guest guest) {
+        return guestService.addGuest(guest);
+    }
+
+    // GET request: http://localhost:8082/api/guests
+    @GetMapping
+    public List<Guest> getAllGuests() {
+        return guestService.getAllGuests();
+    }
 }
--- guest-service/src/main/java/com/hotel/guest_service/service/GuestService.java
+++ guest-service/src/main/java/com/hotel/guest_service/service/GuestService.java
@@ -1,4 +1,25 @@
 package com.hotel.guest_service.service;
 
+import com.hotel.guest_service.model.Guest;
+import com.hotel.guest_service.repo.GuestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GuestService {
+
+    @Autowired
+    private GuestRepository guestRepository;
+
+    // Save a new guest to the database
+    public Guest addGuest(Guest guest) {
+        return guestRepository.save(guest);
+    }
+
+    // Get a list of all guests from the database
+    public List<Guest> getAllGuests() {
+        return guestRepository.findAll();
+    }
 }
